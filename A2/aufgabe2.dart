7018500f6d1fb9eb635f5e7c3c26f675cf861651
Ben Meyer-Meisel <ben.meyermeisel@gmail.com>
2020-12-27 15:04:00 +0100

    implementation of cut algorithm

--- A2/aufgabe2.dart
+++ A2/aufgabe2.dart
@@ -6,6 +6,41 @@ class Skewer {
 
   Skewer(this.bowlNumbers, this.fruits);
 
+  //removes overlapping sections of skewer and returns overlapping skewer (empty lists if nothing is overlapping)
+  Skewer cutOverlap(Skewer skewer) {
+    List<int> overlapBowlNumbers = [];
+    List<String> overlapFruits = [];
+
+    //add overlap
+    for (int i = 0; i < bowlNumbers.length; i++) {
+      for (int j = 0; j < skewer.bowlNumbers.length; j++) {
+        if (bowlNumbers[i] == skewer.bowlNumbers[j]) {
+          overlapBowlNumbers.add(bowlNumbers[i]);
+        }
+        if (fruits[i] == skewer.fruits[j]) {
+          overlapFruits.add(fruits[i]);
+        }
+      }
+    }
+
+    //remove overlapping elements
+    skewer.bowlNumbers.removeWhere((element) {
+      return overlapBowlNumbers.contains(element);
+    });
+    skewer.fruits.removeWhere((element) {
+      return overlapFruits.contains(element);
+    });
+
+    bowlNumbers.removeWhere((element) {
+      return overlapBowlNumbers.contains(element);
+    });
+    fruits.removeWhere((element) {
+      return overlapFruits.contains(element);
+    });
+
+    return Skewer(overlapBowlNumbers, overlapFruits);
+  }
+
   @override
   String toString() {
     return "$bowlNumbers = $fruits";
@@ -32,6 +67,7 @@ void main() async {
     List<String> targetFruits = extractFruitsFrom(lines[1]);
     int nSkewer = int.parse(lines[2]);
     List<Skewer> skewers = [];
+    //create skewers
     for (int n = 3; n < nSkewer * 2 + 3; n += 2) {
       List<int> bowlNumbers = [];
 
@@ -45,8 +81,15 @@ void main() async {
 
       skewers.add(Skewer(bowlNumbers, fruits));
     }
-    print(skewers);
+
+    //TODO handle if wanted fruit is not in map or cannot be solved
+    Map<String, int> solution = assignFruitForBowl(skewers);
   }
+}
+
+Map<String, int> assignFruitForBowl(List<Skewer> skewers) {
+  Map<String, int> fruitForBowl = {};
+  return fruitForBowl;
 }
 
 List<String> extractFruitsFrom(String text) {
