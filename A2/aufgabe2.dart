32f2d765e6b77517edb9c09866d20833a735629b
unknown <ben.meyermeisel@gmail.com>
2020-12-31 18:55:07 +0100

    new examples and roughly determining runtime complexity

--- A2/aufgabe2.dart
+++ A2/aufgabe2.dart
@@ -12,6 +12,7 @@ class Skewer {
   bool get isNotEmpty => bowlNumbers.isNotEmpty;
 
   //removes overlapping sections of skewer and returns overlapping skewer (containing empty lists if nothing is overlapping)
+  //2 Skewer can only have n fruits so O=(n^2), n=fruits //TODO proof
   Skewer cutOverlap(Skewer skewer) {
     List<int> overlapBowlNumbers = [];
     List<String> overlapFruits = [];
@@ -32,6 +33,11 @@ class Skewer {
         }
       }
     }
+
+    if (overlapBowlNumbers.length != overlapFruits.length) {
+      throw "Inconsistent data";
+    }
+
     //remove overlapping elements
     skewer.bowlNumbers.removeWhere((element) {
       return overlapBowlNumbers.contains(element);
@@ -66,6 +72,7 @@ class Skewer {
 //run by using cmd: 1. go in the current dict 2. run: dart aufgabe2.dart
 void main() async {
   List<String> filenames = [
+    "spiesse0.txt", //given example
     "spiesse1.txt",
     "spiesse2.txt",
     "spiesse3.txt",
@@ -74,6 +81,7 @@ void main() async {
     "spiesse6.txt",
     "spiesse7.txt",
     "spiesse8.txt", //for >1 unnoted fruits
+    "spiesse9.txt", //to many unknown fruits
   ];
   for (String filename in filenames) {
     File file =
@@ -108,12 +116,14 @@ void main() async {
   }
 }
 
+//O=(n^3) of assigning combinations, n=fruits dominated by O(reduce())
+//TODO proof
 List<int> solve(List<Skewer> skewers, List<String> targetFruits, int nFruits) {
   List<int> bowls = [];
   List<Skewer> reducedSkewers = reduce(skewers);
 
   //reduction analysis
-  print("Reduced skewers $reducedSkewers");
+  //print("Reduced skewers $reducedSkewers");
 
   //map single items and store combinations in a separate list
   Map<String, int> bowlByFruit = {};
@@ -126,75 +136,89 @@ List<int> solve(List<Skewer> skewers, List<String> targetFruits, int nFruits) {
     }
   }
 
-  //assign pairs
-  for (int i = 0; i < targetFruits.length; i++) {
-    //assign clear pairs
-    if (bowlByFruit.containsKey(targetFruits[i])) {
-      bowls.add(bowlByFruit.putIfAbsent(
-          targetFruits[i], () => throw "No bowl value"));
-      targetFruits.removeAt(i);
-      //do not skip the next item
-      i--;
-    }
-    //try to assign unclear pairs
-    else {
-      for (int j = 0; j < combinations.length; j++) {
-        //test if targetFruit can be find in skewer
-        if (combinations[j].fruits.contains(targetFruits[i])) {
-          //test if every fruit is in the targetFruits
-          combinations[j].fruits.forEach((fruit) {
+  try {
+    //assign pairs O(n)
+    for (int i = 0; i < targetFruits.length; i++) {
+      //assign clear pairs
+      if (bowlByFruit.containsKey(targetFruits[i])) {
+        bowls.add(bowlByFruit.putIfAbsent(
+            targetFruits[i], () => throw "No bowl value"));
+        targetFruits.removeAt(i);
+        //do not skip the next item
+        i--;
+      }
+      //try to assign unclear pairs
+      else {
+        //O(n/2)
+        for (int j = 0; j < combinations.length; j++) {
+          //test if targetFruit can be find in skewer, if not, fruit is not noted
+          //more combinations, lesser runtime complexity. Lesser combinations, more runtime complexity but the current loop has a lesser runtime complexity
+          if (combinations[j].fruits.contains(targetFruits[i])) {
+            //test if every fruit is in the targetFruits
+            //more combinations, lesser runtime complexity. Lesser combinations, more runtime complexity but the current loop has a lesser runtime complexity
+            combinations[j].fruits.forEach((fruit) {
+              int targetFruitsLength = targetFruits.length;
+              /*
+            //easier to read, but adds O(n) to runtime complexity
             if (!targetFruits.contains(fruit)) {
-              throw "To little information to make skewer";
-            }
-            //remove matching fruits
-            targetFruits.removeWhere((targetFruit) => fruit == targetFruit);
-          });
-          //add numbers
-          bowls.addAll(combinations[j].bowlNumbers);
-          //do not skip the next item
-          i--;
-          break; //inner loop
+              throw "unsolvable: $fruit is in a group combination but not wanted in the final skewer";
+            } 
+            */
+              //remove matching fruits
+              targetFruits.removeWhere((targetFruit) => fruit == targetFruit);
+              //if not element is removed,no targetFruit not in group combination
+              if (targetFruitsLength == targetFruits.length) {
+                throw "unsolvable: $fruit is in a group combination but not wanted in the final skewer";
+              }
+            });
+            //add numbers
+            bowls.addAll(combinations[j].bowlNumbers);
+            //do not skip the next item
+            i--;
+            break; //inner loop
+          }
         }
       }
     }
-  }
 
-  //handling if target fruit(s) was not seen on skewers
-  if (targetFruits.isNotEmpty) {
-    //collect noted bowls numbers
-    List<int> notedBowls = [];
-    notedBowls.addAll(bowlByFruit.values);
-    combinations.forEach((combination) {
-      notedBowls.addAll(combination.bowlNumbers);
-    });
-
-    //check if the number of unknown fruits is equal to the target fruits
-    if (targetFruits.length == nFruits - notedBowls.length) {
-      List<int> unnotedBowls = [];
-      for (int i = 1; i <= nFruits; i++) {
-        //bowl not noted
-        if (!notedBowls.contains(i)) unnotedBowls.add(i);
-      }
-      bowls.addAll(unnotedBowls);
-
-      //for analysis of clearly assignable pairs
-      if (targetFruits.length == 1) {
-        bowlByFruit.putIfAbsent(targetFruits[0], () => unnotedBowls[0]);
+    //handling if target fruit(s) was not seen on skewers
+    if (targetFruits.isNotEmpty) {
+      //collect noted bowls numbers
+      List<int> notedBowls = [];
+      notedBowls.addAll(bowlByFruit.values);
+      combinations.forEach((combination) {
+        notedBowls.addAll(combination.bowlNumbers);
+      });
+      //check if the number of unknown fruits is equal to the target fruits
+      if (targetFruits.length == nFruits - notedBowls.length) {
+        List<int> unnotedBowls = [];
+        for (int i = 1; i <= nFruits; i++) {
+          //bowl not noted
+          if (!notedBowls.contains(i)) unnotedBowls.add(i);
+        }
+        bowls.addAll(unnotedBowls);
+        //for analysis of clearly assignable pairs
+        if (targetFruits.length == 1) {
+          bowlByFruit.putIfAbsent(targetFruits[0], () => unnotedBowls[0]);
+        } else {
+          combinations.add(Skewer(unnotedBowls, targetFruits));
+        }
+        targetFruits = [];
       } else {
-        combinations.add(Skewer(unnotedBowls, targetFruits));
+        throw "unsolvable: there are ${targetFruits.length} fruits that are wanted and not seen on a skewer, but ${nFruits - notedBowls.length} unknown fruits in total";
       }
-      targetFruits = [];
-    } else {
-      throw "To little information to make skewer";
     }
+  } catch (e) {
+    throw e;
+  } finally {
+    //analysis
+    print("One to one combinations: $bowlByFruit");
+    print("Many to many combinations: $combinations");
   }
-
-  //analysis
-  print("One to one combinations: $bowlByFruit");
-  print("Many to many combinations: $combinations");
   return bowls;
 }
 
+//O=(m^2)*O(cutOverlap()) of for loops (i = j cuts runtime in half O=1/2(m^2)), m=skwers TODO proof => ignoring insert/removeAt
 List<Skewer> reduce(List<Skewer> skewers) {
   for (int i = 0; i < skewers.length; i++) {
     //j = i because everything < i has already been checked for overlapping parts.
@@ -222,7 +246,10 @@ List<Skewer> reduce(List<Skewer> skewers) {
         else if (skewers[i > j ? j : j + 1].isEmpty) {
           skewers.removeAt(i > j ? j : j + 1);
         }
-        //no skewer empty, no action required
+        //no skewer empty, go one step further (since one item was added to the list, not increasing j would result in a comparison to the just compared skewer)
+        else {
+          j++;
+        }
       }
     }
   }
